ae0bce6b62cb64734396ae6b870f4c3147e563d7
dkxce <25371256+dkxce@users.noreply.github.com>
2023-04-07 15:50:56 +0300

    Add files via upload

--- FileAss.cs
+++ FileAss.cs
@@ -1,18 +1,34 @@
-using System;
+﻿using System;
 using System.IO;
-using System.Diagnostics;
-using System.Runtime;
 using System.Runtime.InteropServices;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Win32;
 
-namespace KMZ_Viewer
+namespace DigitalCertAndSignMaker
 {
     public class FileAss
-    {
+    {        
         public static void SetFileAssociation(string Extension, string Class, string Command, string ExePath)
         {
+            string cls = null;
+            SetFileAssociation(Extension, ref cls, Command, ExePath, Command);
+        }
+
+        public static void SetFileAssociation(string Extension, string Class, string Command, string ExePath, string commandName)
+        {
+            string cls = null;
+            SetFileAssociation(Extension, ref cls, Command, ExePath, commandName);
+        }
+
+        public static void SetFileAssociation(string Extension, ref string Class, string Command, string ExePath)
+        {
+            SetFileAssociation(Extension, ref Class, Command, ExePath, Command);
+        }
+
+        public static void SetFileAssociation(string Extension, ref string Class, string Command, string ExePath, string commandName)
+        {
+            if (string.IsNullOrEmpty(commandName)) commandName = Command;
+            Extension = Extension.Trim('.').ToLower();
+
             try
             {
                 Registry.CurrentUser.OpenSubKey("SOFTWARE\\Classes\\", true)
@@ -21,14 +37,37 @@ namespace KMZ_Viewer
                     .CreateSubKey(Path.GetFileName(ExePath))
                     .SetValue("", "\"" + ExePath + "\"" + " \"%1\"");
 
+                if (string.IsNullOrEmpty(Class))
+                    using (RegistryKey User_Ext = Registry.ClassesRoot.CreateSubKey("." + Extension))
+                        Class = User_Ext.GetValue("", "").ToString();
+                if (string.IsNullOrEmpty(Class))
+                    using (RegistryKey User_Ext = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\." + Extension))
+                        Class = User_Ext.GetValue("", "").ToString();
+
+                if (string.IsNullOrEmpty(Class)) Class = Extension.ToUpper() + "File";
+
+                using (RegistryKey User_Ext = Registry.ClassesRoot.CreateSubKey("." + Extension))
+                    User_Ext.SetValue("", Class);
+                using (RegistryKey User_Ext = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\." + Extension))
+                    User_Ext.SetValue("", Class);
+
                 using (RegistryKey User_Classes = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Classes\\", true))
-                using (RegistryKey User_Ext = User_Classes.CreateSubKey("." + Extension))
                 using (RegistryKey User_FileClass = User_Classes.CreateSubKey(Class))
-                using (RegistryKey User_Command = User_FileClass.CreateSubKey("shell").CreateSubKey(Command).CreateSubKey("command"))
+                using (RegistryKey User_Command = User_FileClass.CreateSubKey("shell").CreateSubKey(Command))
+                using (RegistryKey User_Execute = User_Command.CreateSubKey("command"))
                 {
-                    User_Ext.SetValue("", Class, RegistryValueKind.String);
-                    User_Classes.SetValue("", Class, RegistryValueKind.String);
-                    User_Command.SetValue("", "\"" + ExePath + "\"" + " \"%1\"");
+                    User_Execute.SetValue("", "\"" + ExePath + "\"" + " \"%1\"");
+                    User_Command.SetValue("", commandName);
+                    User_Command.SetValue("Icon", ExePath);
+                };
+
+                using (RegistryKey User_FileClass = Registry.ClassesRoot.CreateSubKey(Class, true))
+                using (RegistryKey User_Command = User_FileClass.CreateSubKey("shell").CreateSubKey(Command))
+                using (RegistryKey User_Execute = User_Command.CreateSubKey("command"))
+                {
+                    User_Execute.SetValue("", "\"" + ExePath + "\"" + " \"%1\"");
+                    User_Command.SetValue("", commandName);
+                    User_Command.SetValue("Icon", ExePath);
                 };
             }
             catch (Exception excpt)
@@ -48,19 +87,14 @@ namespace KMZ_Viewer
                     .SetValue("", "\"" + ExePath + "\"" + " \"%1\"");
 
             }
-            catch (Exception excpt)
+            catch (Exception ex)
             {
-                //Your code here
-            }
+            };
         }
 
         public static void UpdateExplorer()
         {
-            try
-            {
-                SHChangeNotify(0x08000000, 0x0000, IntPtr.Zero, IntPtr.Zero);
-            }
-            catch { };
+            try { SHChangeNotify(0x08000000, 0x0000, IntPtr.Zero, IntPtr.Zero); } catch { };
         }
 
         [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
